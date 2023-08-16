93a69cb9c652d43b2fb0abfb863b94e73b66d3d6
Joel Spinelli <Joel.Spinelli@gds.ey.com>
2023-08-16 14:04:20 -0300

    windows

--- Client/src/app/guards/type-not-found.guard.ts
+++ Client/src/app/guards/type-not-found.guard.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRoute, CanActivate, Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { ActivatedRouteSnapshot, CanActivate, Router } from '@angular/router';
+import { Observable, of } from 'rxjs';
 import { TypeService } from '../services/type.service';
 import { map } from 'rxjs/operators';
 
@@ -8,25 +8,16 @@ import { map } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class TypeNotFoundGuard implements CanActivate {
-  constructor(private typeService: TypeService, private router: Router, private route: ActivatedRoute) { }
+  constructor(private typeService: TypeService, private router: Router) { }
 
-  canActivate(): Observable<boolean> {
-    let artPieceType: string; 
-
-    this.route.paramMap.subscribe(params => {
-      artPieceType = params.get('type') || '';
-    })
-    
+  canActivate(route: ActivatedRouteSnapshot): Observable<boolean> {
     return this.typeService.types$.pipe(
       map(types => {
-        if (types) {
-          if (types.some(type => type.name === artPieceType)) {
-            return true;
-          } else {
-            this.router.navigateByUrl('page-not-found');
-          }
-        }
+        if (!types) return false;
 
+        if (types.some(type => type.name === route.paramMap.get('type'))) return true;
+
+        this.router.navigateByUrl('page-not-found');
         return false;
       })
     );
